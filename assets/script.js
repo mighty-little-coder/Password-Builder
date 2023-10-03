e1696361d250ecf4161ffcb6bc986ca460fce97f
mighty-little-coder <144641610+mighty-little-coder@users.noreply.github.com>
2023-10-03 18:41:06 -0400

    finished project

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,3 @@
-// Assignment code here
-
-// All that is necessary is create function and return value from it. Then get input from user and create string.
-// Maybe include a "copy to clipboard" feature
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -16,15 +10,10 @@ function writePassword() {
 
 }
 
-// function generatePassword() {
-//   return //generated password.
-// }
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-var charLength = window.prompt("How many characters would you like your password to contain? (Min. characters of 8. Max characters of 128.)");
+function generatePassword() {
+  var charLength = window.prompt("How many characters would you like your password to contain? (Min. characters of 8. Max characters of 128.)");
 console.log(charLength);
-
+// if charLength < 8 || charLength > 128
 var charSpecial = window.confirm("Click OK to confirm including special characters.")
 console.log(charSpecial);
 
@@ -37,46 +26,53 @@ console.log(charCaselow)
 var charCasehigh = window.confirm("Click OK to confirm including uppercase characters.")
 console.log(charCasehigh)
 
-var charArrayCaselow = ["abcdefghijklmnopqrstuvwxyz"]
+var charArrayCaselow = "abcdefghijklmnopqrstuvwxyz"
 
-var charArrayCasehigh = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+var charArrayCasehigh = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-var numArray = ["123456789"]
+var numArray = "0123456789"
 
+var charArrayspecial = "!@#$%^&*()"
 
-// 8-128 characters
-// Lower / Upper case letters
-// Numeric / Alphabetic
-// Special Characters / Typical Characters
+var chosenVariables = ""
 
-// How many Characters would you like your password to contain?
-// Click OK to confirm including special characters.
-// Click OK to confirm including numeric characters.
-// Click OK to confirm including lowercase characters.
-// Click OK to confirm including uppercase characters.
+if (charSpecial) {
+  chosenVariables = chosenVariables + charArrayspecial
+}
+if (charNumeric) {
+  chosenVariables = chosenVariables + numArray
+}
+if (charCaselow) {
+  chosenVariables = chosenVariables + charArrayCaselow
+}
+if (charCasehigh) {
+  chosenVariables = chosenVariables + charArrayCasehigh
+}
+console.log(chosenVariables)
 
+var createdPassword = ""
 
+for (let i = 0; i < charLength; i++) {
+  createdPassword = createdPassword + chosenVariables.charAt(Math.floor(Math.random()*chosenVariables.length))
+}
 
+  return createdPassword //generated password.
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
+function myFunction() {
+  // Get the text field
+  var copyText = document.getElementById("password");
 
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 128); // For mobile devices
 
-// AS AN employee with access to sensitive data
-// I WANT to randomly generate a password that meets certain criteria
-// SO THAT I can create a strong password that provides greater security
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
 
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-// Mock-Up
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
+}
+
